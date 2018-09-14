f3091190279be6788b1df24fe61ca8551ef82f4b
merlepolscher <43272708+merlepolscher@users.noreply.github.com>
2018-09-14 15:55:06 +0200

    Add files via upload

--- info.php
+++ info.php
@@ -1,4 +1,4 @@
 <?
 $titel='BACK TO THE ROOTS - POLAROID LÄSST GRÜßEN';
-$beschreibung='';
+$beschreibung='Der Charme von Sofortbildkameras hat vor allem die jüngere Generation wieder ergriffen. Nach einer Flaute von mehr als 20 Jahren tauchen jetzt immer mehr Modelle auf, die den Schnappschuss wieder direkt in die Hand liefern. So auch die farbenfrohe Instax mini 8, die laut Aussage des Herstellers Fuji auf keiner Party fehlen darf.';
 ?>
