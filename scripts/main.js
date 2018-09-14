c5ad23c0d553a527628ebe29187354e49b58c85c
merlepolscher <43272708+merlepolscher@users.noreply.github.com>
2018-09-14 18:21:46 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,11 @@
 document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
+        if(this.getAttribute('href') != "#"){
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
+        }
     });
 });
 
